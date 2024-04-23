3235338dfcd60f01c4a4a388f735ff88a132b4ed
Karthikeyan-amk <karthikeyananbarasu7@gmail.com>
2024-04-23 17:59:15 +0530

    change the url to our db url

---
+++ initializer/database.go
@@ -0,0 +1,41 @@
+package initializer
+
+import (
+	"context"
+	"log"
+
+	"github.com/karthikeyan-amk/go-curd/models"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var TestCollection *mongo.Collection
+
+func init() {
+	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI("url").SetServerAPIOptions(serverAPI)
+	client, err := mongo.Connect(context.Background(), opts)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB: ", err)
+	}
+	TestCollection = client.Database("test").Collection("test")
+	log.Println("Connected to MongoDB!")
+}
+
+func CreateMovie(user *models.Movie) error {
+	// Insert user into the database
+	_, err := TestCollection.InsertOne(context.Background(), user)
+	if err != nil {
+		return err
+	}
+
+	log.Println("User created successfully!")
+	return nil
+}
